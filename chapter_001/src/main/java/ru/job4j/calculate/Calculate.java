9570da6464c2dee592eb744e10a8a943f88ef65b
mtambv <mtambv@gmail.com>
2019-06-01 16:21:11 -0500

    1.3. JavaDoc. Code convention[#143580]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
+  /**
+  * Calulate
+  *
+  * @author Mstislav Tambovtsev (mtambv@gmail.com)
+  */
 
 public class Calculate {
+	/**
+	*Main.
+	* @param args-argd.
+	*/
 	
 	public static void main (String [] args ){
 		System.out.println ("Hello World");
