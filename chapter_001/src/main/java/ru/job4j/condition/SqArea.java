65ac5b63b1c4aac76066ec1477eb692dd07fb8ff
mtambv <mtambv@gmail.com>
2019-06-08 15:00:33 -0500

    3.5. Стороны прямоугольника.[#143569]-1

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,19 +1,20 @@
 package ru.job4j.condition;
 
 public class SqArea {
+    /**
+     * method square
+     *
+     * returns length, width, and square of rectangle
+     * if we know perimeter and length/width ratio
+     *
+     * @param p perimeter
+     * @param k length/width ratio
+     * @return w -длинна
+     * @return h -высота
+     * @return s - площадь полученного прямоугольника
+     */
+
     public double square(int p, int k) {
-        /**
-         * method square
-         *
-         * returns length, width, and square of rectangle
-         * if we know perimeter and length/width ratio
-         *
-         * @param p perimeter
-         * @param k length/width ratio
-         * @return w -длинна
-         * @return h -высота
-         * @return s - площадь полученного прямоугольника
-         */
 
         double h = (p / 2) / (k + 1);
         double w = h * k;
