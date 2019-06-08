640b9db5ecb75ed573e9074f16341c330d487afe
mtambv <mtambv@gmail.com>
2019-06-08 17:48:02 -0500

    4.4. Максимум из трех чисел[#143540]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * @author Mstislav Tambovtsev (mtambv@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MultiMax {
+    /**
+     * Mthod max takes 3 numbers and returns the highest
+     *
+     * @param first  input parameter
+     * @param second input parameter
+     * @param third  input parameter
+     * @return the highest number
+     */
+
+    public int max(int first, int second, int third) {
+
+        int result = third > (first > second ? first : second) ? third : ((first > second) ? first : second);
+        return result;
+    }
+}
