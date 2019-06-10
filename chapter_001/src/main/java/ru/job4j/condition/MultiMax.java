42af477cb4f08622726e066fa0f34bc139f70d60
mtambv <mtambv@gmail.com>
2019-06-09 19:28:25 -0500

    5.1. Подсчет суммы чётных чисел в диапазоне[#143547]-1

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -18,7 +18,8 @@ public class MultiMax {
 
     public int max(int first, int second, int third) {
 
-        int result = third > (first > second ? first : second) ? third : ((first > second) ? first : second);
+        int result = third > (first > second ? first : second) ? third :
+                ((first > second) ? first : second);
         return result;
     }
 }
