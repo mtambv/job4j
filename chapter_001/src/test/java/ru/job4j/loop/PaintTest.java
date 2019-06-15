61d11d31c617c269495ef531335551761a015a1c
mtambv <mtambv@gmail.com>
2019-06-15 14:30:26 -0500

    5.4. Построить пирамиду в псевдографике.[#143550]-1

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -45,5 +45,20 @@ public class PaintTest {
                 )
         );
     }
-
+    @Test
+    public void whenPyramid4() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
 }
