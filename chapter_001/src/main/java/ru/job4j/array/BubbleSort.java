87a653d448128cc35f091f88f1c12ccbddcc38b9
mtambv <mtambv@gmail.com>
2019-06-18 22:52:55 -0500

    6.5. Создать программу для сортировки массива методом перестановки.[#143560]

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+/**
+ * @author Mstislav Tambovtsev (mtambv@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class BubbleSort {
+    /**
+     * method sort takes:
+     * @param array as input
+     * @return sorted array
+     */
+
+    public int[] sort(int[] array) {
+
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (array[j] > array[j + 1]) {
+
+                    // swap arr[j+1] and arr[i]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+        return array;
+    }
+
+
+
+
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Mstislav Tambovtsev (mtambv@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class BubbleSortTest {
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        // тест, проверяющий сортировку массива из 10 элементов методом пузырька.
+        BubbleSort  bubbleSort = new BubbleSort();
+        int[] input = new int[] {3, 2, 4, 5, 6, 7, 8, 9, 0, 1};
+        int[] result = bubbleSort.sort(input);
+        int[] expect = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(result, is(expect));
+
+    }
+}
