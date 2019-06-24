60888cf05fc6c1b7969361d27ea3577f4eb96047
mtambv <mtambv@gmail.com>
2019-06-23 23:13:30 -0500

    6.7. Квадратный массив заполнен true или false по диагоналям.[#143558]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -7,16 +7,25 @@ package ru.job4j.array;
  */
 
 public class MatrixCheck {
+    /**
+     * Method mono
+     * @param data array
+     * @return if all item in the aray are "true" of "false"
+     * in 2 diagonals
+     *
+     */
 
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        int size = data.length - 1;
-        for (int out = 0; out != size; out++) {
-            for (int inner = 0; inner != size; inner++) {
-                if (data[out][inner] != data[out + 1][inner + 1] ) {
-                    result = false;
-                    break;
-                }
+        int rowCount = data.length - 1;
+
+        for (int i = 0, j = 0; i < rowCount; i++, j++) {
+            if (data[i][j] != data[i + 1][j + 1]) {
+                result = false;
+            }
+            if (data[rowCount][j] != data[rowCount - 1][j + 1]) {
+                result = false;
+                break;
             }
         }
         return result;
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -29,7 +29,7 @@ public class MatrixCheckTest {
         MatrixCheck check = new MatrixCheck();
         boolean[][] input = new boolean[][] {
                 {true, true, false},
-                {false, true, true},
+                {false, false, true},
                 {true, false, false}
         };
         boolean result = check.mono(input);
@@ -40,14 +40,14 @@ public class MatrixCheckTest {
     public void whenEvenDataMonoByTrueThenTrue() {
         MatrixCheck check = new MatrixCheck();
         boolean[][] input = new boolean[][] {
-                {true, true, true, true,},
+                {true, true, true, true},
                 {false,true, true, true},
                 {true, true, true, true},
-                {true, true, true, true}
+                {true, true, true, false}
 
         };
         boolean result = check.mono(input);
-        assertThat(result, is(true));
+        assertThat(result, is(false));
     }
     @Test
     public void whenEvenDataNotMonoByTrueThenFalse() {
