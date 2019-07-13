88327c92c75b19be32a21994d2077e46b0a24c74
mtambv <mtambv@gmail.com>
2019-07-12 21:17:03 -0500

    Рефакторинг - Площадь треугольника.[#143647]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/TriangleTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // Создаем объект треугольник.
+        Triangle triangle = new Triangle(new Point(0,0),new Point(0,2),new Point(2,0));
+        // Вычисляем площадь.
+        double result = triangle.area (0, 0, 0, 2, 2, 0);
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
