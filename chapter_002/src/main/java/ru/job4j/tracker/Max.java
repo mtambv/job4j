f596e4a38ab0109e312ccc9557308307b5bca5ee
mtambv <mtambv@gmail.com>
2019-07-13 22:58:15 -0500

    Перегрузить метод max для трех чисел[#143649]

--- chapter_002/src/main/java/ru/job4j/tracker/Max.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Max.java
@@ -2,20 +2,19 @@ package ru.job4j.tracker;
 
 public class Max {
 
-
     public int max(int first, int second) {
         int result = first >=  second ? first : second;
         return result;
     }
     public int max(int first, int second, int third) {
-        int temp = first > second ? first : second;
-        int result = third > temp ? third : temp;
-        return result;
+        int temp = max (second, third);
+        return max (first, temp);
     }
-    public int max(int first, int second, int third,int fourth) {
-        int temp1 = first > second ? first : second;
-        int temp2 = third > fourth ? third : fourth;
-        int result = temp1 > temp2 ? temp1 : temp2;
+    public int max(int first, int second, int third, int fourth) {
+        int temp1 = max (first, second);
+        int temp2 = max (third,fourth);
+        int result = max (temp1,temp2);
         return result;
     }
 }
+
