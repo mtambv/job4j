7c97ffdb5d4e2276a8181917230abab1e3340a58
mtambv <mtambv@gmail.com>
2019-08-12 23:14:37 -0500

    Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#143632]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -31,7 +31,8 @@ public class StartUI {
 
     /**
      * Конструтор инициализирующий поля.
-     * @param input ввод данных.
+     *
+     * @param input   ввод данных.
      * @param tracker хранилище заявок.
      */
     public StartUI(Input input, Tracker tracker) {
@@ -48,20 +49,27 @@ public class StartUI {
             this.showMenu();
             String answer = this.input.ask("Введите пункт меню : ");
             int select = Integer.parseInt(answer);
-switch (select) {
-            case 0 : this.createItem();
-                break;
-            case 1 : this.showAllItems();
-                 break;
-            case 2 : this.editItem();
-                break;
-            case 3 : this.deleteItem();
-                break;
-            case 4 : this.findItemById();
-                break;
-            case 5 : this.findItemByName();
-                break;
-            case 6 : exit = true;
+            switch (select) {
+                case 0:
+                    this.createItem();
+                    break;
+                case 1:
+                    this.showAllItems();
+                    break;
+                case 2:
+                    this.editItem();
+                    break;
+                case 3:
+                    this.deleteItem();
+                    break;
+                case 4:
+                    this.findItemById();
+                    break;
+                case 5:
+                    this.findItemByName();
+                    break;
+                case 6:
+                    exit = true;
             }
         }
     }
@@ -78,13 +86,15 @@ switch (select) {
         this.tracker.add(item);
         System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
     }
+
     private void showAllItems() {
         System.out.println("------------ Displaying all items in the Tracker -----------");
-        Item[]items = this.tracker.findAll();
-        for (Item item: items){
-            System.out.println (item);
+        Item[] items = this.tracker.findAll();
+        for (Item item : items) {
+            System.out.println(item);
         }
     }
+
     private void editItem() {
         System.out.println("------------ Enter item you want to delete --------------");
         String name = this.input.ask("Введите имя заявки :");
@@ -92,48 +102,58 @@ switch (select) {
         String desc = this.input.ask("Enter description you wish to edit:");
         long time = new java.util.Date().getTime();
         Item item = new Item(name, desc, time);
-        if(this.tracker.replace(id, item)) {
-            System.out.println ("------ заявка обновлена_-------");
-        }else{
-            System.out.println ("-------итем не найден ---------");
+        if (this.tracker.replace(id, item)) {
+            System.out.println("------ заявка обновлена_-------");
+        } else {
+            System.out.println("-------итем не найден ---------");
         }
     }
 
     private void deleteItem() {
         String id = this.input.ask("Введите id заявки :");
-        if(this.tracker.delete(id)) {
-            System.out.println ("------ итем удален -------");
-        }else{
-            System.out.println ("-------итем не найден ---------");
+        if (this.tracker.delete(id)) {
+            System.out.println("------ итем удален -------");
+        } else {
+            System.out.println("-------итем не найден ---------");
         }
     }
 
     private void findItemById() {
         String id = this.input.ask("Введите id заявки :");
-        System.out.println (this.tracker.findById (id));
-
+        Item item = (this.tracker.findById(id));
+        if (item != null) {
+            System.out.println(item);
+        }
+        else {System.out.println("-------итем не найден ---------");
+        }
     }
-    private void findItemByName() {
-        String key = this.input.ask("Введите id заявки :");
-        System.out.println (this.tracker.findByName(key));
+        private void findItemByName () {
+            String key = this.input.ask("Введите id заявки :");
+            Item[] items = this.tracker.findByName(key);
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        }
 
+        private boolean exit () {
+        return true ;
+        }
+
+        private void showMenu () {
+            System.out.print("Меню.\n 0. Add new Item \n 1. Show all items\n 2. Edit item \n " +
+                    "3.Delete item \n 4. Find item by Id \n 5. Find items by name \n 6. Exit Program \n Select: ");
+
+        }
+
+        /**
+         * Запускт программы.
+         * @param args
+         */
+        public static void main (String[]args){
+            ConsoleInput input = new ConsoleInput();
+            Tracker tracker = new Tracker();
+            String name = input.ask("Please enter the task's name:");
+            //tracker.add(new Task (name, "first desc"));
+            new StartUI(new ConsoleInput(), new Tracker()).init();
+        }
     }
-
-    private void showMenu() {
-        System.out.print("Меню.\n 0. Add new Item \n 1. Show all items\n 2. Edit item \n " +
-                "3.Delete item \n 4. Find item by Id \n 5. Find items by name \n 6. Exit Program \n Select: ");
-
-    }
-
-    /**
-     * Запускт программы.
-     * @param args
-     */
-    public static void main(String[] args) {
-        ConsoleInput input = new ConsoleInput();
-        Tracker tracker = new Tracker();
-        String name = input.ask("Please enter the task's name:");
-        //tracker.add(new Task (name, "first desc"));
-        new StartUI(new ConsoleInput(), new Tracker()).init();
-    }
-}
