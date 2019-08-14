e203fd45687f9c35e4386ceec6ec86912e1d3684
mtambv <mtambv@gmail.com>
2019-08-13 22:28:19 -0500

    profession

---
+++ chapter_002/src/main/java/ru/job4j/chapter1ReFactor/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.chapter1ReFactor;
+
+public class Max {
+
+    public int max(int first, int second) {
+        int result = first >=  second ? first : second;
+        return result;
+    }
+    public int max(int first, int second, int third) {
+        int temp = max (second, third);
+        return max (first, temp);
+    }
+    public int max(int first, int second, int third, int fourth) {
+        int temp1 = max (first, second);
+        int temp2 = max (third,fourth);
+        int result = max (temp1,temp2);
+        return result;
+    }
+}
+
