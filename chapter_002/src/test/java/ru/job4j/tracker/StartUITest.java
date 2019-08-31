735bfbdef68fb7db6fdf1d3164b1b2f45eba03e9
mtambv <mtambv@gmail.com>
2019-08-30 21:39:08 -0500

    Используя класс StubInput написать тесты проверяющие поведение пользователя[#143633]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,16 +1,24 @@
 package ru.job4j.tracker;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
 
-    @Test
-    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+    @Before
+    public void initialActions (){
         Tracker tracker = new Tracker();     // создаём Tracker
         Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
         new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
+    }
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        //Tracker tracker = new Tracker();     // создаём Tracker
+        //Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
+        //new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
         assertThat(tracker.findAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
     }
     @Test
