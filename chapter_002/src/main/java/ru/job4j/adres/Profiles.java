0ad61fa0bc7dd91e7916e6c09a4dd7e9d8304e79
mtambv <mtambv@gmail.com>
2019-12-14 22:16:33 -0600

    Уникальность элементов и сортировка.[#143501]

--- chapter_002/src/main/java/ru/job4j/adres/Profiles.java
+++ chapter_002/src/main/java/ru/job4j/adres/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.adres;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +9,11 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(
                 Profile::getAddress
-        ).collect(Collectors.toList());
+        ).sorted(new Comparator<Address>() {
+            @Override
+            public int compare(Address address, Address t1) {
+                return address.getCity().compareToIgnoreCase(t1.getCity());
+            }
+        }).distinct().collect(Collectors.toList());
     }
 }
