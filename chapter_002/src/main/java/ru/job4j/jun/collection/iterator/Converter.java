b3c9e7f2055c11639ad6a92588f5a4821e9c833f
mtambv <mtambv@gmail.com>
2020-01-16 22:19:21 -0600

    5.1.4. Создать convert(Iterator<Iterator>)[#194256]

---
+++ chapter_002/src/main/java/ru/job4j/jun/collection/iterator/Converter.java
@@ -0,0 +1,29 @@
+package ru.job4j.jun.collection.iterator;
+import java.util.Iterator;
+
+public class Converter {
+    Iterator<Integer> iterator = null;
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        iterator = it.next();
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                if (it.hasNext() && !iterator.hasNext()) {
+                    iterator = it.next();
+                }
+                return iterator.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+
+                if (!iterator.hasNext()) {
+                    iterator = it.next();
+                }
+                return iterator.next();
+            }
+        };
+
+    }
+}
